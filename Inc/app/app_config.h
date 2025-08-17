47acf7f8d542ab9edb03df17b97bf4ea4d637d4e
Andrii Dobrus <58091875+MuffinKing-jpeg@users.noreply.github.com>
2025-08-17 22:28:13 +0300

    Moved ADC related configuration data to app_config.h

--- Inc/app/app_config.h
+++ Inc/app/app_config.h
@@ -2,10 +2,24 @@
 #define APP_CONFIG_H
 
 #include <stdint.h>
+
+#include "board.h"
 #include "dma.h"
 #include "tim1.h"
 
 // #define USE_POWER_LATCH
+
+#ifdef BOARD_ADC_CHANNEL_QTY
+#undef BOARD_ADC_CHANNEL_QTY
+#endif
+#define BOARD_ADC_CHANNEL_QTY 2
+
+#define BOARD_ADC_BATT    ADC_CHANNEL_4
+#define BOARD_ADC_LDR     ADC_CHANNEL_15
+
+#define BOARD_ADC_CHANNEL_LIST (BOARD_ADC_BATT | BOARD_ADC_LDR)
+
+static uint32_t ADC_Data[BOARD_ADC_CHANNEL_QTY] = {0};
 
 typedef enum
 {
@@ -28,7 +42,7 @@ static const APP_ConfigTIMTrigger_TypeDef ADC_Config = {
 };
 
 static const TIM1_ConfigTypeDef TIM1_Config = {
-    .PSC = 0,
+    .PSC = 0x0080,
     .ARR = 0xFFFF,
     .CCR1 = 0,
     .CCR2 = 0,
